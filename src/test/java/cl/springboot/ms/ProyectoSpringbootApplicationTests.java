81d726fba33a3c95d53f18cb8e1ca2fc602b42b2
Patricio Toro <patricio.andres21@gmail.com>
2023-09-04 21:09:59 -0300

    feat: creación proyecto base

---
+++ src/test/java/cl/springboot/ms/ProyectoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springboot.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
